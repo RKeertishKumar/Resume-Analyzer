ac7cbaff00d764391bd82ff45b4774f84707563a
RKeertishKumar <141417594+RKeertishKumar@users.noreply.github.com>
2024-08-01 15:31:04 +0530

    Improvements on current job roles features

--- genai_api.py
+++ genai_api.py
@@ -9,3 +9,12 @@ def gemini_api_response(resume_info,path_info):
     response = model.generate_content(f"Roadmap for {path_info} role in 5 points based on weakness in {resume_info}")
     suggesetion = response.text
     return suggesetion
+
+def gemini_api_response_job_role(resume_info,path_info,job_role):
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    response = model.generate_content(f"Roadmap for {path_info} role in 5 points based on weakness in {resume_info} for {job_role}")
+    suggesetion = response.text
+    return suggesetion
