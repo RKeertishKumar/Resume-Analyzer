8d3718c97857b5fcdeb80d9fb4d34d93fca6ffcc
RKeertishKumar <141417594+RKeertishKumar@users.noreply.github.com>
2024-08-01 21:40:15 +0530

    Updated with testscript

--- genai_api.py
+++ genai_api.py
@@ -1,6 +1,13 @@
 import google.generativeai as genai
 import os
 
+def clean_response(response):
+    if response.startswith("```python"):
+        response = response[len("```python"):].strip()
+    if response.endswith("```"):
+        response = response[:-len("```")].strip()
+    return response
+
 def gemini_api_response(resume_info,path_info):
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
@@ -17,4 +24,14 @@ def gemini_api_response_job_role(resume_info,path_info,job_role):
 
     response = model.generate_content(f"Roadmap for {path_info} role in 5 points based on weakness in {resume_info} for {job_role}")
     suggesetion = response.text
+    return suggesetion
+
+def gemini_api_flashcards(text):
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    response = model.generate_content(f"give only a python list with 5 important interview question and answers for topics within {text} in this format with question as key and answer as value with a list of dictionaries")
+    suggesetion = response.text
+    suggesetion = clean_response(suggesetion)
     return suggesetion
---
+++ testscript.py
@@ -0,0 +1,6 @@
+from genai_api import gemini_api_flashcards
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(gemini_api_flashcards("Python"))
